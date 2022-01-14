fd4c27f1d27a8aac8e4b1395eac258d3b56342e2
Aleksey Lavrenyuk <aleksey_lavrenyuk@mail.ru>
2022-01-14 18:54:27 +0200

    removed reading environments variables from env

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,9 +6,6 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = new Dotenv();
-$dotenv->load('../.env');
-
 $containerBuilder = (new ContainerBuilder())
     ->useAnnotations(true)
     ->useAutowiring(true)
